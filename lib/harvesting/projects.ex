1289e98381103bff1be3de8cc9843ef1089ff44f
Simon Hohl <simon.hohl@dainst.org>
2020-11-02 11:45:26 +0100

    Harvest project details
    ...instead of just the initial result list, which is missing joins.

--- lib/harvesting/projects.ex
+++ lib/harvesting/projects.ex
@@ -4,25 +4,32 @@ defmodule Argos.Harvesting.Projects do
 
   def start() do
     "#{@base_url}/api/projects"
-    |> run_query
+    |> run_harvest
   end
 
   def start(%DateTime{} = datetime) do
     query = URI.encode_query(%{ since: datetime })
 
     "#{@base_url}/api/projects?#{query}"
-    |> run_query
+    |> run_harvest
   end
 
-  def run_query(url) do
-    url
-    |> HTTPoison.get
-    |> handle_result
-  end
+  defp run_harvest(url) do
+    query_result =
+      url
+      |> HTTPoison.get
+      |> handle_result
 
-  defp handle_result({:ok, %HTTPoison.Response{status_code: 200, body: body}} = _response) do
-    Poison.decode!(body)["data"]
+    # TODO: Switch to project code after Erga got updated
+    query_result["data"]
+    |> Enum.map(&get_details(&1["id"]))
     |> Enum.each(&put_project(&1))
+  end
+
+  defp get_details(id) do
+    "#{@base_url}/api/projects/#{id}"
+      |> HTTPoison.get
+      |> handle_result
   end
 
   defp put_project(project) do
@@ -30,4 +37,9 @@ defmodule Argos.Harvesting.Projects do
     |> HTTPoison.put!(Poison.encode!(project), [{"Content-Type", "application/json"}])
   end
 
+  defp handle_result({:ok, %HTTPoison.Response{status_code: 200, body: body}} = _response) do
+    Poison.decode!(body)
+  end
+
+  # TODO: Handle error results
 end
