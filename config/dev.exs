6fa7009e3f76cad006ee6d77de0c7b358982e325
David Neugebauer <david.neugebauer@posteo.de>
2020-12-03 16:21:36 +0100

    Add a script and a general process for harvesting chronontology data (sd-857).

--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,8 @@ use Mix.Config
 config :argos,
   elasticsearch_url: "localhost:9200/argos",
   projects_url: "localhost:4000",
-  projects_harvest_interval: 1000 * 5 # 5 seconds
+  projects_harvest_interval: 1000 * 5, # 5 seconds
+
+  chronontology_url: "https://chronontology.dainst.org/data",
+  chronontology_batch_size: 10,
+  chronontology_harvest_interval: 1000 * 10 # 10 seconds
