7f80fa93aff5355d27c33f0e9ec2426228690f4d
Simon Hohl <simon.hohl@dainst.org>
2020-12-09 10:36:50 +0100

    Set project harvest interval to 5 minutes

--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,4 @@ use Mix.Config
 config :argos,
   elasticsearch_url: "localhost:9200/argos",
   projects_url: "localhost:4000",
-  projects_harvest_interval: 1000 * 5 # 5 seconds
+  projects_harvest_interval: 1000 * 60 * 5 # 5 minutes
