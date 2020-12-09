10a249723e5762c1e7009eef3d183b81866a1a3b
Simon Hohl <simon.hohl@dainst.org>
2020-12-09 13:19:14 +0100

    Add missing comma in configuration

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :argos,
   elasticsearch_url: "localhost:9200/argos",
   projects_url: "localhost:4000",
-  projects_harvest_interval: 1000 * 60 * 5 # 5 minutes
+  projects_harvest_interval: 1000 * 60 * 5, # 5 minutes
 
   chronontology_url: "https://chronontology.dainst.org/data",
   chronontology_batch_size: 10,
