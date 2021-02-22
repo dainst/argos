e774ab7e8493ca6cb2cbb3bd90da9ab17340350f
Simon Hohl <simon.hohl@dainst.org>
2021-02-22 10:19:40 +0100

    Fix config

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :argos,
-  elasticsearch_url: "localhost:9200/argos",
+  elasticsearch_url: "localhost:9200/argos"
 
   # projects_url: "ocalhost:4000",
   # projects_harvest_interval: 1000 * 60 * 5 # 5 minutes
