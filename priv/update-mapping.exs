5b64cab2710790344f60a9c479e1776e2da094df
Simon Hohl <simon.hohl@dainst.org>
2021-02-23 09:31:51 +0100

    Remove intermediate inspect

--- priv/update-mapping.exs
+++ priv/update-mapping.exs
@@ -2,7 +2,6 @@ mapping = File.read!("priv/elasticsearch-mapping.json")
 
 "#{Application.get_env(:argos, :elasticsearch_url)}"
 |> HTTPoison.delete()
-|> IO.inspect
 
 "#{Application.get_env(:argos, :elasticsearch_url)}"
 |> HTTPoison.put()
