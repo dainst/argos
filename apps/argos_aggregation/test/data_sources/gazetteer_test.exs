f2ad1a4c0cc8c0184d71d60beb075728529e29bb
Simon Hohl <simon.hohl@dainst.org>
2021-05-26 13:33:00 +0200

    Remove unused index creation/deletion in  test

--- apps/argos_aggregation/test/data_sources/gazetteer_test.exs
+++ apps/argos_aggregation/test/data_sources/gazetteer_test.exs
@@ -9,20 +9,6 @@ defmodule ArgosAggregation.GazetteerTest do
   alias ArgosAggregation.Gazetteer.DataProvider
   alias ArgosAggregation.Gazetteer.Harvester
 
-  setup_all %{} do
-    Logger.info("starting tests")
-    Logger.info("creating test index")
-    TestClient.create_test_index()
-    TestClient.put_mapping()
-
-    on_exit(fn ->
-      Logger.info("delete test index")
-      TestClient.delete_test_index()
-    end)
-    :ok
-  end
-
-
   test "run gazzetteer data provider get_by_date" do
     [data|_] =
       DataProvider.get_by_date(~D[2021-01-01])
@@ -36,8 +22,6 @@ defmodule ArgosAggregation.GazetteerTest do
       |> Enum.to_list()
     assert %Place{} = data
   end
-
-
 
   test "run gazetteer data provider get_by_id" do
     data = DataProvider.get_by_id("2048575")
