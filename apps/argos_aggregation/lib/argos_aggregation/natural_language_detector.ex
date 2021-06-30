290fe54235c7d741e32fae9cd397eb4f16903b87
Simon Hohl <simon.hohl@dainst.org>
2021-06-30 14:43:18 +0200

    Remove naive retry logic for language detector

--- apps/argos_aggregation/lib/argos_aggregation/natural_language_detector.ex
+++ apps/argos_aggregation/lib/argos_aggregation/natural_language_detector.ex
@@ -5,27 +5,19 @@ defmodule ArgosAggregation.NaturalLanguageDetector do
   def get_language_key(string, threshold \\ 0.9)
   def get_language_key(string, threshold) when is_binary(string) do
     detection_result =
-      try do
-        string
-        |> Tongue.detect()
-        |> Enum.filter(fn ({_key, score}) ->
-          score > threshold
-        end)
-        |> Enum.sort(fn ({_key, score}, :desc) ->
-          score
-        end)
-        |> List.first()
-      catch
-        :exit, _value ->
-          :retry
-        end
+      string
+      |> Tongue.detect()
+      |> Enum.filter(fn ({_key, score}) ->
+        score > threshold
+      end)
+      |> Enum.sort(fn ({_key, score}, :desc) ->
+        score
+      end)
+      |> List.first()
 
     case detection_result do
       {lang_code, _score} ->
         Atom.to_string(lang_code)
-      :retry ->
-        Logger.warning("Detection process timed out, retrying to parse #{string}")
-        get_language_key(string, threshold)
       nil ->
         # Nothing over threshold
         ""
@@ -38,23 +30,17 @@ defmodule ArgosAggregation.NaturalLanguageDetector do
   end
 
   def get_language_keys_with_scores(string) when is_binary(string) do
-    try do
-      string
-      |> Tongue.detect()
-      |> Enum.sort(fn ({_key, value}, :desc) ->
-        value
-      end)
-      |> Enum.map(fn({key, score}) ->
-        %{
-          lang: Atom.to_string(key),
-          score: score
-        }
-      end)
-    catch
-      :exit, _value ->
-        Logger.warning("Detection process timed out, retrying to parse #{string}")
-        get_language_keys_with_scores(string)
-      end
+    string
+    |> Tongue.detect()
+    |> Enum.sort(fn ({_key, value}, :desc) ->
+      value
+    end)
+    |> Enum.map(fn({key, score}) ->
+      %{
+        lang: Atom.to_string(key),
+        score: score
+      }
+    end)
   end
 
   def get_language_keys_with_scores(_no_string) do
