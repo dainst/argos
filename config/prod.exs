d52c26b5dbb3292eb9589566edb20c8d900fed91
Simon Hohl <simon.hohl@dainst.org>
2021-06-30 14:34:26 +0200

    Change prod log level

--- config/prod.exs
+++ config/prod.exs
@@ -11,3 +11,5 @@ config :argos_aggregation,
   elasticsearch_url: "elasticsearch:9200",
   index_name: "argos",
   elasticsearch_mapping_path: "/elasticsearch-mapping.json"
+
+config :logger, level: :info
