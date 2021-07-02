7dc1f5784f9664275a59a8e1d49932029c893495
Simon Hohl <simon.hohl@dainst.org>
2021-07-02 08:18:42 +0200

    Remove :tongue from extra_applications

--- apps/argos_aggregation/mix.exs
+++ apps/argos_aggregation/mix.exs
@@ -22,7 +22,7 @@ defmodule ArgosAggregation.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :tongue],
+      extra_applications: [:logger],
       mod: {ArgosAggregation.Application, []}
     ]
   end
