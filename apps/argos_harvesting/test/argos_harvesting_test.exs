faddf26b574621b46902de03b68ea40286bbb00a
Simon Hohl <simon.hohl@dainst.org>
2021-09-01 09:50:09 +0200

    Correctly setup harvesting tests

--- apps/argos_harvesting/test/argos_harvesting_test.exs
+++ apps/argos_harvesting/test/argos_harvesting_test.exs
@@ -4,24 +4,47 @@ defmodule ArgosHarvestingTest do
 
   alias ArgosHarvesting.{
     Gazetteer,
-    Thesauri
+    Thesauri,
+    Bibliography
   }
 
-  test "gazetteer harvester harvests by date" do
-    result =
-      Date.utc_today()
-      |> Date.add(-7)
-      |> Gazetteer.run_harvest()
+  alias ArgosHarvesting.TestHelpers
 
-    assert :ok = result
-  end
+  describe "elastic search tests" do
 
-  test "thesauri harvester harvests by date" do
-    result =
-      Date.utc_today()
-      |> Date.add(-7)
-      |> Thesauri.run_harvest()
+    setup %{} do
+      TestHelpers.create_index()
 
-    assert :ok = result
+      on_exit(fn ->
+        TestHelpers.remove_index()
+      end)
+      :ok
+    end
+
+    test "gazetteer harvester harvests by date" do
+      result =
+        Date.utc_today()
+        |> Date.add(-7)
+        |> Gazetteer.run_harvest()
+
+      assert :ok = result
+    end
+
+    test "thesauri harvester harvests by date" do
+      result =
+        Date.utc_today()
+        |> Date.add(-7)
+        |> Thesauri.run_harvest()
+
+      assert :ok = result
+    end
+
+    test "bibliography harvester harvests by date" do
+      result =
+        DateTime.now!("Etc/UTC")
+        |> Bibliography.run_harvest()
+
+      assert [] = result
+    end
   end
 end
