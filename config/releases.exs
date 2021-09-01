b639fd2bf42d07cf2ad5c9a2c0e6e1475e1aaa44
Simon Hohl <simon.hohl@dainst.org>
2021-09-01 09:32:53 +0200

    Add some comments to release.exs config

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,11 @@
-import Config
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
 
-require Logger
+# General application runtime (!) configuration for release builds
+import Config
 
 config :argos_api,
   host_url: System.fetch_env!("HOST")
