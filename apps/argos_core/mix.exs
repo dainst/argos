726e3f57064e17ecb1879b21a8c9fd5f78f2706b
Simon Hohl <simon.hohl@dainst.org>
2021-09-02 12:31:07 +0200

    Set new versions

--- apps/argos_core/mix.exs
+++ apps/argos_core/mix.exs
@@ -4,7 +4,7 @@ defmodule ArgosCore.MixProject do
   def project do
     [
       app: :argos_core,
-      version: "0.1.0",
+      version: "0.1.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/argos_harvesting/mix.exs
+++ apps/argos_harvesting/mix.exs
@@ -4,7 +4,7 @@ defmodule ArgosHarvesting.MixProject do
   def project do
     [
       app: :argos_harvesting,
-      version: "0.1.0",
+      version: "0.1.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
