193847e4cbcaeeb27b075497fd37a7d79ce3c80d
Simon Hohl <simon.hohl@dainst.org>
2021-09-10 11:34:33 +0200

    Add mailer test

--- apps/argos_core/test/argos_core_test.exs
+++ apps/argos_core/test/argos_core_test.exs
@@ -1,4 +1,21 @@
 defmodule ArgosCoreTest do
   use ExUnit.Case
   doctest ArgosCore
+
+  test "mailer creates email based on configuration" do
+    subject = "Test subject"
+    body = "Test body"
+
+    %{
+      subject: ^subject,
+      text_body: ^body,
+      from: from,
+      to: to
+      } =
+      %{subject: "Test subject", text_body: "Test body"}
+      |> ArgosCore.Mailer.send_email()
+
+    assert Application.get_env(:argos_core, :mail_sender) == from
+    assert Application.get_env(:argos_core, :mail_recipient) == to
+  end
 end
