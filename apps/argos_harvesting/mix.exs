0d9f30f39b63fb971d809a678dcbcc7ff4c9c852
Simon Hohl <simon.hohl@dainst.org>
2021-09-15 12:28:49 +0200

    Increase version number

--- apps/argos_harvesting/mix.exs
+++ apps/argos_harvesting/mix.exs
@@ -4,7 +4,7 @@ defmodule ArgosHarvesting.MixProject do
   def project do
     [
       app: :argos_harvesting,
-      version: "0.2.2",
+      version: "0.2.3",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
